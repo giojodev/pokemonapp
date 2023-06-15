09297f82a6a1b67c160419d402354a536d7662c8
Giovanni Orozco <80933087+giojodev@users.noreply.github.com>
2023-06-14 23:13:05 -0600

    Cambios en css local

--- app/components/Navigations.tsx
+++ app/components/Navigations.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import styles from './Navigation.module.css';
 
 const links = [
   {
@@ -29,9 +30,9 @@ const links = [
 
 export function Navigation() {
   return (<>
-    <header>
-      <nav>
-        <ul>
+    <header className={styles.header}>
+      <nav >
+        <ul className={styles.navigation}>
           {links.map(({ label, route }) => (
             <li key={route}>
               <Link href={route}>{label}</Link>
